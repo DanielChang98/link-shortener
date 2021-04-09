a14820e436f778c9bf4d982c11bde5d17d846ed7
Phang Wei <phangwei@coingecko.com>
2021-04-09 17:24:24 +0800

    Add secured password to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_021809) do
+ActiveRecord::Schema.define(version: 2021_04_09_084350) do
 
   create_table "links", force: :cascade do |t|
     t.string "lookup_code"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2021_03_31_021809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "counter", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "visitors", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(name: "Example User", email: "user@example.com", 
+                      password: "foobar", password_confirmation: "foobar")}
+
+  it "should be valid" do
+    assert user.valid?
+  end
+
+  it "name should be present" do
+    user.name = " "
+    expect(user.valid?).to be false  
+  end
+
+  it "email should be present" do
+    user.email = " "
+    expect(user.valid?).to be false  
+  end
+
+  it "name should not be too long" do
+    user.name = "a" * 51
+    expect(user.valid?).to be false  
+  end
+
+  it "email should not be too long" do
+    user.email = "a" * 244 + "@example.com"
+    expect(user.valid?).to be false  
+  end
+
+  it "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      user.email = address
+      expect(user.valid?).to be true  
+    end
+  end
+
+  it "email addresses should be unique" do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user.valid?).to be false  
+  end
+  
+  it "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    user.email = mixed_case_email
+    user.save
+    expect(mixed_case_email.downcase).to eq user.reload.email  
+  end
+
+  it "password should be present (nonblank)" do
+    user.password = user.password_confirmation = " " * 6
+    expect(user.valid?).to eq false
+  end
+
+  it "password should have a minimum length" do
+    user.password = user.password_confirmation = "a" * 5
+    expect(user.valid?).to eq false
+  end
 end
